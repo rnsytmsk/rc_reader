1f3dab7325d36dec1775e17508d8d78ec6a8886f
rnsytmsk <rnsytmsk@gmail.com>
2024-04-29 23:57:37 +0900

    add mfc_app

---
+++ mfc_app/mfc_app/mfc_app.h
@@ -0,0 +1,32 @@
+﻿
+// mfc_app.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CmfcappApp:
+// このクラスの実装については、mfc_app.cpp を参照してください
+//
+
+class CmfcappApp : public CWinApp
+{
+public:
+	CmfcappApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcappApp theApp;
